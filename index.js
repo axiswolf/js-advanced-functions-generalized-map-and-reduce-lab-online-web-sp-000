aea84598924f7e63773022508cfe152289d5c993
Karen V <karenvo@usc.edu>
2020-01-08 09:03:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ function map(array, cb) {
     return result
 }
 
-function reduce(array, cb, value=0) {
+function reduce(array, cb, value) {
+  if (typeof value == "undefined") {
+    if (array.some(isNaN)) value = true 
+    else value = 0
+  }
   for (let x of array) {
     value = cb(value,x,array)
   }
